1a1488b3c59c1c8180b8c97c260a200700b787ac
Nathaniel Thoma <nthoma3851@gmail.com>
2025-12-22 18:36:02 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
+"""
+Module Name: main.py
+Description: 
+Author: Nathaniel Thoma
+Date: 2025-12-19
+"""
+
 import parse_ghosts
 import parse_ghost_general
+import parse_equipment
+import parse_equipment_general
+import sys
 
-parser_a = parse_ghosts.Parser()
-data = parser_a.extract_to_json()
+extractors = [
+    parse_ghosts.Extractor(),
+    parse_ghost_general.Extractor(),
+    parse_equipment.Extractor(),
+    parse_equipment_general.Extractor()
+]
 
-parser_b = parse_ghost_general.Parser()
-data = parser_b.extract_to_json()
+if len(sys.argv) > 1:
+    arg = sys.argv[1]
+else:
+    arg = "all"
+
+if arg == "all":
+    for extractor in extractors:
+        extractor.extract_to_json()
+elif arg == "ghosts":
+    extractors[0].extract_to_json()
+elif arg == "ghost_general":
+    extractors[1].extract_to_json()
+elif arg == "equipment":
+    extractors[2].extract_to_json()
+elif arg == "equipment_general":
+    extractors[3].extract_to_json()
+else:
+    print(f"Unknown argument: {arg}")
